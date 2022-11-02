742c8f73d0dd2acd31ade7caea0a94eea09eb9eb
CodeDan <ThomasDanielJose@outlook.com>
2022-11-02 00:44:00 -0500

    modalListos

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ liElements.forEach(i => i.addEventListener('click',()=>{
     btn.classList.remove('active')
 }))
 
-// ============================ OBTENER INFORMACION DE PRODUCTOS ATRAVES DE API =============
+// ============================ OBTENER INFORMACION DE PRODUCTOS ATRAVES DE API, EJECUCION DE MODAL =============
 
 const obtenerInformacionIphone = async()=>{
 
@@ -29,6 +29,7 @@ const obtenerInformacionIphone = async()=>{
         const array = datos.iphoneInfo;
 
         let iphones = ''
+
         array.forEach(iphone => {
             iphones += `
                 <article class="product">
@@ -47,17 +48,94 @@ const obtenerInformacionIphone = async()=>{
 
         document.getElementById('productos').innerHTML = iphones
 
-        const btnBuy = document.querySelectorAll('.btnBuy')
+        const btnComprar = document.querySelectorAll('.btnBuy')
+        
+        const btnMostrarMas = document.querySelectorAll('.btnAddCar')
 
-        const btnShowMore = document.querySelectorAll('.btnAddCar')
 
-        btnBuy.forEach(i => i.addEventListener('click',()=>{
-            alert('Compra Exitosa')
+        btnMostrarMas.forEach (elemento => elemento.addEventListener('click',()=>{
+
+            const objeto_A_array = Object.values(btnMostrarMas);
+
+            const posicion_del_boton_en_un_producto = objeto_A_array.indexOf(elemento);
+
+            const productoNombre = array[posicion_del_boton_en_un_producto].name;
+
+            const img = array[posicion_del_boton_en_un_producto].img;
+
+            const precio = array[posicion_del_boton_en_un_producto].precio
+
+            const descripcion = array[posicion_del_boton_en_un_producto].description
+
+            const modalCont = document.querySelector('.modal');
+
+            const modalHTML = `
+                <div class="cuadro">
+                    <img src="${img}" alt="">
+                    <div class="contTextProduct">
+                        <span class="nombreModal">${productoNombre}</span>
+                        <span class="precioModal">${precio}</span>
+                        <span>${descripcion}</span>
+                        <span class="descripcionSpan">Decripcion Tecnica :</span>
+                        <ul class="descripcion">
+                            <li>Almacenamiento : 42Gb</li>
+                            <li>Procesador : Core i4</li>
+                            <li>Camara Frontal : 15 pixeles</li>
+                        </ul>
+                        <i class="fa-solid fa-xmark"></i>
+                    </div>
+                </div>
+            `
+            modalCont.innerHTML = modalHTML;
+
+            modalCont.classList.toggle('active');
+
+            const btnCloseModal = document.querySelector('.fa-xmark')
+
+            btnCloseModal.addEventListener('click',()=>{
+
+                modalCont.classList.remove('active');
+
+            })
         }))
 
-        btnShowMore.forEach(i => i.addEventListener('click',()=>{
-            console.log(i);
+        btnComprar.forEach(elemento => elemento.addEventListener('click',()=>{
+
+            const objeto_A_array = Object.values(btnComprar);
+
+            const posicion_del_boton_en_un_producto = objeto_A_array.indexOf(elemento)
+
+            const productoNombre = array[posicion_del_boton_en_un_producto].name;
+
+            const precio = array[posicion_del_boton_en_un_producto].precio
+
+            const modalCont = document.querySelector('.modal');
+
+            const modalHTML = `
+                <div class="cuadro">
+                    <img src="https://cdn-icons-png.flaticon.com/512/7778/7778643.png" alt="">
+                    <div class="contTextProduct">
+                        <span class="nombreModal">Producto comprado : ${productoNombre}</span>
+                        <span class="precioModal">${precio}</span>
+                        <i class="fa-solid fa-xmark"></i>
+                    </div>
+                </div>
+            `
+            modalCont.innerHTML = modalHTML;
+
+            modalCont.classList.toggle('active');
+
+            const btnCloseModal = document.querySelector('.fa-xmark')
+
+            btnCloseModal.addEventListener('click',()=>{
+
+                modalCont.classList.remove('active');
+
+            })
+
         }))
+
+        
 
     } catch (error) {
 
@@ -67,6 +145,7 @@ const obtenerInformacionIphone = async()=>{
 }
 
 obtenerInformacionIphone()
+
 
 //============================ MODO OSCURO ==========================
 
